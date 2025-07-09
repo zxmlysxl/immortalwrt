8e327755c0e8817e75af92db5db8d834a3e65531
zxmlysxl <zxmlysxl@gmail.com>
2025-07-09 09:17:57 +0800

    add files dir

---
+++ include/ccache.mk
@@ -0,0 +1,2 @@
+CCACHE_SLOPPINESS = file_macro,locale,time_macros
+CCACHE_BASEDIR = $(TOPDIR)
--- zxm_mk.sh
+++ zxm_mk.sh
@@ -10,8 +10,20 @@ declare -A week_map=(
 )
 echo "${date_str} ${time_str} by 上网的蜗牛 ${week_map[$week_num]}" > compile_date.txt
 
+# 计算并发数
+calc_jobs() {
+  local mem_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+  local mem_mb=$(( mem_kb / 1024 ))
+  local jobs=$(( mem_mb / 1500 ))
+  echo $(( jobs > 0 ? jobs : 1 ))  # 至少1个线程
+}
+
 #开始编译
-make clean
+#make clean
+#make dirclean
 git pull
 ./scripts/feeds update -a && ./scripts/feeds install -a
-make V=s -j$(nproc)
+make download -j8
+make -j$(calc_jobs) V=s 2>&1 | tee build.log  #增量编译
+#make world -j$(calc_jobs) V=s 2>&1 | tee build.log  #完整编译
+
